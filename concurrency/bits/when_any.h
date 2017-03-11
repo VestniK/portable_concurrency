dbf9ddc927b77341bea485d86d15f13661729e1a
Sergey Vidyuk <sir.vestnik@gmail.com>
2017-03-11 13:37:20 +0700

    More when_any tests with bugfixes

--- concurrency/bits/when_any.h
+++ concurrency/bits/when_any.h
@@ -43,66 +43,6 @@ struct are_futures<F0, F...>: std::integral_constant<
   is_future<F0>::value && are_futures<F...>::value
 > {};
 
-template<typename Sequence>
-struct find;
-
-template<typename Future>
-struct find<std::vector<Future>> {
-  static std::size_t index_of_ready(const std::vector<Future>& seq) {
-    std::size_t res = 0;
-    for (const auto& f: seq) {
-      if (f.is_ready())
-        return res;
-      ++res;
-    }
-    return static_cast<std::size_t>(-1);
-  }
-};
-
-template<typename... Futures>
-struct find<std::tuple<Futures...>> {
-  static std::size_t index_of_ready(const std::tuple<Futures...>& seq) {
-    return index_of_ready(seq, std::make_index_sequence<sizeof...(Futures)>{});
-  }
-
-  template<size_t... I>
-  static std::size_t index_of_ready(const std::tuple<Futures...>& seq, std::index_sequence<I...>) {
-    bool statuses[] = {std::get<I>(seq).is_ready()...};
-    std::size_t res = 0;
-    for (bool ready: statuses) {
-      if (ready)
-        return res;
-      ++res;
-    }
-    return static_cast<std::size_t>(-1);
-  }
-};
-
-template<typename Sequence>
-class when_any_state:
-  public shared_state<when_any_result<Sequence>>,
-  public continuation
-{
-public:
-  when_any_state(Sequence&& futures): futures_(std::move(futures)) {}
-
-  // threadsafe
-  void invoke() override {
-    if (satisfied_.exchange(true))
-      return;
-    this->emplace(when_any_result<Sequence>{
-      find<Sequence>::index_of_ready(futures_),
-      std::move(futures_)
-    });
-  }
-
-  Sequence& futures() {return futures_;}
-
-private:
-  Sequence futures_;
-  std::atomic<bool> satisfied_ = {false};
-};
-
 template<typename T>
 shared_state<T>* state_of(future<T>& f) {
   return f.state_.get();
@@ -115,14 +55,114 @@ shared_state<T>* state_of(shared_future<T>& f) {
 
 void swallow(...) {}
 
-template<typename Sequence, size_t... I>
-auto when_any(Sequence&& seq, std::index_sequence<I...>) {
-  auto state = std::make_shared<when_any_state<Sequence>>(std::move(seq));
-  swallow(
-    (state_of(std::get<I>(state->futures()))->add_continuation(state), 0)...
-  );
-  return future<when_any_result<Sequence>>{state};
-}
+template<typename Sequence>
+struct sequence_traits;
+
+template<typename Future>
+struct sequence_traits<std::vector<Future>> {
+  static std::size_t index_of_ready(const std::vector<Future>& seq) {
+    std::size_t res = 0;
+    for (const auto& f: seq) {
+      if (f.is_ready())
+        return res;
+      ++res;
+    }
+    return static_cast<std::size_t>(-1);
+  }
+
+  static std::size_t size(const std::vector<Future>& seq) {
+    return seq.size();
+  }
+
+  static void attach_continuation(
+    std::vector<Future>& seq,
+    const std::shared_ptr<continuation>& cnt
+  ) {
+    for (const auto& f: seq)
+      ::experimental::concurrency_v1::detail::state_of(f)->add_continuation(cnt);
+  }
+};
+
+template<typename... Futures>
+struct sequence_traits<std::tuple<Futures...>> {
+  static std::size_t index_of_ready(const std::tuple<Futures...>& seq) {
+    return index_of_ready(seq, std::make_index_sequence<sizeof...(Futures)>{});
+  }
+
+  static std::size_t size(const std::tuple<Futures...>& seq) {
+    return sizeof...(Futures);
+  }
+
+  static void attach_continuation(
+    std::tuple<Futures...>& seq,
+    const std::shared_ptr<continuation>& cnt
+  ) {
+    attach_continuation(seq, cnt, std::make_index_sequence<sizeof...(Futures)>{});
+  }
+
+private:
+  template<size_t... I>
+  static std::size_t index_of_ready(const std::tuple<Futures...>& seq, std::index_sequence<I...>) {
+    bool statuses[] = {std::get<I>(seq).is_ready()...};
+    std::size_t res = 0;
+    for (bool ready: statuses) {
+      if (ready)
+        return res;
+      ++res;
+    }
+    return static_cast<std::size_t>(-1);
+  }
+
+  template<size_t... I>
+  static void attach_continuation(
+    std::tuple<Futures...>& seq,
+    const std::shared_ptr<continuation>& cnt,
+    std::index_sequence<I...>
+  ) {
+    swallow((
+      ::experimental::concurrency_v1::detail::state_of(std::get<I>(seq))->add_continuation(cnt), 0
+    )...);
+  }
+};
+
+template<typename Sequence>
+class when_any_state:
+  public shared_state<when_any_result<Sequence>>,
+  public continuation
+{
+public:
+  when_any_state(Sequence&& futures):
+    futures_(std::move(futures)),
+    barrier_(sequence_traits<Sequence>::size(futures_) + 1)
+  {}
+
+  // threadsafe
+  void invoke() override {
+    if (barrier_.fetch_sub(1) != 1)
+      return;
+    this->emplace(when_any_result<Sequence>{
+      sequence_traits<Sequence>::index_of_ready(futures_),
+      std::move(futures_)
+    });
+  }
+
+  static std::shared_ptr<shared_state<when_any_result<Sequence>>> make(Sequence&& seq) {
+    const int seq_sz = sequence_traits<Sequence>::size(seq);
+    auto state = std::make_shared<when_any_state<Sequence>>(std::move(seq));
+    sequence_traits<Sequence>::attach_continuation(state->futures_, state);
+    if (state->barrier_.fetch_sub(seq_sz) > seq_sz)
+      return state;
+    state->emplace(when_any_result<Sequence>{
+      sequence_traits<Sequence>::index_of_ready(state->futures_),
+      std::move(state->futures_)
+    });
+    return state;
+  }
+
+private:
+  Sequence futures_;
+  std::atomic<int> barrier_;
+};
 
 } // namespace detail
 
@@ -136,15 +176,14 @@ future<when_any_result<std::tuple<>>> when_any() {
 template<typename... Futures>
 auto when_any(Futures&&... futures) ->
   std::enable_if_t<
-    sizeof...(Futures) != 0 && detail::are_futures<std::decay_t<Futures>...>::value,
+    detail::are_futures<std::decay_t<Futures>...>::value,
     future<when_any_result<std::tuple<std::decay_t<Futures>...>>>
   >
 {
   using Sequence = std::tuple<std::decay_t<Futures>...>;
-  return detail::when_any(
-    Sequence{std::forward<Futures>(futures)...},
-    std::make_index_sequence<sizeof...(Futures)>{}
-  );
+  return future<when_any_result<Sequence>>{detail::when_any_state<Sequence>::make(
+    Sequence{std::forward<Futures>(futures)...}
+  )};
 }
 
 } // inline namespace concurrency_v1
--- test/when_any.hpp
+++ test/when_any.hpp
@@ -8,6 +8,8 @@
 
 namespace {
 
+class WhenAnyTest: public ::testing::TestWithParam<size_t> {};
+
 TEST(WhenAnyTest, empty_sequence) {
   auto res_fut = experimental::when_any();
   ASSERT_TRUE(res_fut.valid());
@@ -121,5 +123,133 @@ TEST(WhenAnyTest, single_error_shared_future) {
   ASSERT_TRUE(std::get<0>(res.futures).is_ready());
   EXPECT_RUNTIME_ERROR(std::get<0>(res.futures), "future with error");
 }
+
+TEST_P(WhenAnyTest, multiple_futures) {
+  experimental::promise<int> p0;
+  auto raw_f0 = p0.get_future();
+  experimental::promise<std::string> p1;
+  auto raw_f1 = p1.get_future().share();
+  experimental::promise<std::unique_ptr<int>> p2;
+  auto raw_f2 = p2.get_future();
+  experimental::promise<void> p3;
+  auto raw_f3 = p3.get_future().share();
+  experimental::promise<future_tests_env&> p4;
+  auto raw_f4 = p4.get_future();
+
+  auto f = experimental::when_any(
+    std::move(raw_f0),
+    raw_f1,
+    std::move(raw_f2),
+    raw_f3,
+    std::move(raw_f4)
+  );
+  ASSERT_TRUE(f.valid());
+  EXPECT_FALSE(raw_f0.valid());
+  EXPECT_TRUE(raw_f1.valid());
+  EXPECT_FALSE(raw_f2.valid());
+  EXPECT_TRUE(raw_f3.valid());
+  EXPECT_FALSE(raw_f4.valid());
+  EXPECT_FALSE(f.is_ready());
+
+  size_t idx_first = GetParam();
+  switch (idx_first) {
+  case 0: p0.set_value(345); break;
+  case 1: p1.set_value("hello world"); break;
+  case 2: p2.set_value(std::make_unique<int>(42)); break;
+  case 3: p3.set_value(); break;
+  case 4: p4.set_value(*g_future_tests_env); break;
+  }
+  ASSERT_TRUE(f.is_ready());
+
+  auto res = f.get();
+  EXPECT_EQ(res.index, idx_first);
+  EXPECT_EQ(std::tuple_size<decltype(res.futures)>::value, 5u);
+
+  ASSERT_TRUE(std::get<0>(res.futures).valid());
+  ASSERT_TRUE(std::get<1>(res.futures).valid());
+  ASSERT_TRUE(std::get<2>(res.futures).valid());
+  ASSERT_TRUE(std::get<3>(res.futures).valid());
+  ASSERT_TRUE(std::get<4>(res.futures).valid());
+
+  ASSERT_TRUE(std::get<0>(res.futures).is_ready() || idx_first != 0);
+  ASSERT_TRUE(std::get<1>(res.futures).is_ready() || idx_first != 1);
+  ASSERT_TRUE(std::get<2>(res.futures).is_ready() || idx_first != 2);
+  ASSERT_TRUE(std::get<3>(res.futures).is_ready() || idx_first != 3);
+  ASSERT_TRUE(std::get<4>(res.futures).is_ready() || idx_first != 4);
+
+  switch (idx_first) {
+  case 0: EXPECT_EQ(std::get<0>(res.futures).get(), 345); break;
+  case 1: EXPECT_EQ(std::get<1>(res.futures).get(), "hello world"s); break;
+  case 2: EXPECT_EQ(*std::get<2>(res.futures).get(), 42); break;
+  case 3: EXPECT_NO_THROW(std::get<3>(res.futures).get()); break;
+  case 4: EXPECT_EQ(&std::get<4>(res.futures).get(), g_future_tests_env); break;
+  }
+}
+
+TEST_P(WhenAnyTest, multiple_futures_one_initially_ready) {
+  experimental::promise<int> p0;
+  experimental::promise<std::string> p1;
+  experimental::promise<std::unique_ptr<int>> p2;
+  experimental::promise<void> p3;
+  experimental::promise<future_tests_env&> p4;
+
+  experimental::shared_future<int> raw_f0 = p0.get_future();
+  experimental::future<std::string> raw_f1 = p1.get_future();
+  experimental::shared_future<std::unique_ptr<int>> raw_f2 = p2.get_future();
+  experimental::future<void> raw_f3 = p3.get_future();
+  experimental::shared_future<future_tests_env&> raw_f4 = p4.get_future();
+
+  size_t idx_first = GetParam();
+  switch (idx_first) {
+  case 0: p0.set_value(345); break;
+  case 1: p1.set_value("hello world"); break;
+  case 2: p2.set_value(std::make_unique<int>(42)); break;
+  case 3: p3.set_value(); break;
+  case 4: p4.set_value(*g_future_tests_env); break;
+  }
+
+  auto f = experimental::when_any(
+    raw_f0,
+    std::move(raw_f1),
+    raw_f2,
+    std::move(raw_f3),
+    raw_f4
+  );
+  ASSERT_TRUE(f.valid());
+  EXPECT_TRUE(raw_f0.valid());
+  EXPECT_FALSE(raw_f1.valid());
+  EXPECT_TRUE(raw_f2.valid());
+  EXPECT_FALSE(raw_f3.valid());
+  EXPECT_TRUE(raw_f4.valid());
+
+  ASSERT_TRUE(f.is_ready());
+
+  auto res = f.get();
+  EXPECT_EQ(res.index, idx_first);
+  EXPECT_EQ(std::tuple_size<decltype(res.futures)>::value, 5u);
+
+  ASSERT_TRUE(std::get<0>(res.futures).valid());
+  ASSERT_TRUE(std::get<1>(res.futures).valid());
+  ASSERT_TRUE(std::get<2>(res.futures).valid());
+  ASSERT_TRUE(std::get<3>(res.futures).valid());
+  ASSERT_TRUE(std::get<4>(res.futures).valid());
+
+  ASSERT_TRUE(std::get<0>(res.futures).is_ready() || idx_first != 0);
+  ASSERT_TRUE(std::get<1>(res.futures).is_ready() || idx_first != 1);
+  ASSERT_TRUE(std::get<2>(res.futures).is_ready() || idx_first != 2);
+  ASSERT_TRUE(std::get<3>(res.futures).is_ready() || idx_first != 3);
+  ASSERT_TRUE(std::get<4>(res.futures).is_ready() || idx_first != 4);
+
+  switch (idx_first) {
+  case 0: EXPECT_EQ(std::get<0>(res.futures).get(), 345); break;
+  case 1: EXPECT_EQ(std::get<1>(res.futures).get(), "hello world"s); break;
+  case 2: EXPECT_EQ(*std::get<2>(res.futures).get(), 42); break;
+  case 3: EXPECT_NO_THROW(std::get<3>(res.futures).get()); break;
+  case 4: EXPECT_EQ(&std::get<4>(res.futures).get(), g_future_tests_env); break;
+  }
+}
+
+size_t first_idx_vals[] = {0, 1, 2, 3, 4};
+INSTANTIATE_TEST_CASE_P(AllVals, WhenAnyTest, ::testing::ValuesIn(first_idx_vals));
 
 } // anonymous namespace
