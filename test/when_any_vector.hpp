99fc09b9eb6c3f33a1be8a38b5b5323ff5578fd0
Sergey Vidyuk <sir.vestnik@gmail.com>
2017-04-08 23:20:01 +0700

    Extra unit tests for the when_any over iterators

--- test/when_any_vector.hpp
+++ test/when_any_vector.hpp
@@ -1,12 +1,65 @@
 #pragma once
 
 #include <algorithm>
+#include <list>
 
 #include <gtest/gtest.h>
 
 #include <concurrency/future>
 
 namespace {
+
+TEST(WhenAnyVectorTest, empty_sequence) {
+  std::list<experimental::future<int>> empty;
+  auto f = experimental::when_any(empty.begin(), empty.end());
+
+  ASSERT_TRUE(f.valid());
+  ASSERT_TRUE(f.is_ready());
+
+  auto res = f.get();
+  EXPECT_EQ(res.index, static_cast<std::size_t>(-1));
+  EXPECT_EQ(res.futures.size(), 0u);
+}
+
+TEST(WhenAnyVectorTest, single_future) {
+  experimental::promise<std::string> p;
+  auto raw_f = p.get_future();
+  auto f = experimental::when_any(&raw_f, &raw_f + 1);
+  ASSERT_TRUE(f.valid());
+  EXPECT_FALSE(raw_f.valid());
+  EXPECT_FALSE(f.is_ready());
+
+  p.set_value("Hello");
+  ASSERT_TRUE(f.is_ready());
+
+  auto res = f.get();
+  EXPECT_EQ(res.index, 0u);
+  EXPECT_EQ(res.futures.size(), 1u);
+
+  ASSERT_TRUE(res.futures[0].valid());
+  ASSERT_TRUE(res.futures[0].is_ready());
+  EXPECT_EQ(res.futures[0].get(), "Hello");
+}
+
+TEST(WhenAnyVectorTest, single_shared_future) {
+  experimental::promise<std::unique_ptr<int>> p;
+  auto raw_f = p.get_future().share();
+  auto f = experimental::when_any(&raw_f, &raw_f + 1);
+  ASSERT_TRUE(f.valid());
+  EXPECT_TRUE(raw_f.valid());
+  EXPECT_FALSE(f.is_ready());
+
+  p.set_value(std::make_unique<int>(42));
+  ASSERT_TRUE(f.is_ready());
+
+  auto res = f.get();
+  EXPECT_EQ(res.index, 0u);
+  EXPECT_EQ(res.futures.size(), 1u);
+
+  ASSERT_TRUE(res.futures[0].valid());
+  ASSERT_TRUE(res.futures[0].is_ready());
+  EXPECT_EQ(*res.futures[0].get(), 42);
+}
 
 TEST(WhenAnyVectorTest, simple) {
   experimental::promise<int> ps[5];
@@ -23,10 +76,11 @@ TEST(WhenAnyVectorTest, simple) {
   EXPECT_FALSE(f.is_ready());
 
   ps[3].set_value(42);
-  EXPECT_TRUE(f.is_ready());
+  ASSERT_TRUE(f.is_ready());
 
   auto res = f.get();
   EXPECT_EQ(res.index, 3u);
+  EXPECT_EQ(res.futures.size(), 5u);
   std::size_t idx = 0;
   for (auto& fc: res.futures)
   {
