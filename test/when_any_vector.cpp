17d0fc50ac700530967b80fa054111bfc419663a
Sergey Vidyuk <sir.vestnik@gmail.com>
2017-04-12 19:47:30 +0700

    More when_any over iterators tests

--- test/when_any_vector.cpp
+++ test/when_any_vector.cpp
@@ -129,7 +129,7 @@ TEST(WhenAnyVectorTest, single_error_shared_future) {
   EXPECT_RUNTIME_ERROR(res.futures[0], "panic");
 }
 
-TEST(WhenAnyVectorTest, simple) {
+TEST(WhenAnyVectorTest, multiple_futures) {
   experimental::promise<int> ps[5];
   experimental::future<int> fs[5];
 
@@ -142,6 +142,8 @@ TEST(WhenAnyVectorTest, simple) {
   auto f = experimental::when_any(std::begin(fs), std::end(fs));
   ASSERT_TRUE(f.valid());
   EXPECT_FALSE(f.is_ready());
+  for (const auto& fi: fs)
+    EXPECT_FALSE(fi.valid());
 
   ps[3].set_value(42);
   ASSERT_TRUE(f.is_ready());
@@ -153,9 +155,98 @@ TEST(WhenAnyVectorTest, simple) {
   for (auto& fc: res.futures)
   {
     ASSERT_TRUE(fc.valid());
-    EXPECT_EQ(fc.is_ready(), idx++ == 3u);
+    EXPECT_EQ(fc.is_ready(), idx++ == res.index);
   }
-  EXPECT_EQ(res.futures[3].get(), 42);
+  EXPECT_EQ(res.futures[res.index].get(), 42);
+}
+
+TEST(WhenAnyVectorTest, multiple_shared_futures) {
+  experimental::promise<std::string> ps[5];
+  experimental::shared_future<std::string> fs[5];
+
+  std::transform(
+    std::begin(ps), std::end(ps),
+    std::begin(fs),
+    std::mem_fn(&experimental::promise<std::string>::get_future)
+  );
+
+  auto f = experimental::when_any(std::begin(fs), std::end(fs));
+  ASSERT_TRUE(f.valid());
+  EXPECT_FALSE(f.is_ready());
+  for (const auto& fi: fs)
+    EXPECT_TRUE(fi.valid());
+
+  ps[2].set_value("42");
+  ASSERT_TRUE(f.is_ready());
+
+  auto res = f.get();
+  EXPECT_EQ(res.index, 2u);
+  EXPECT_EQ(res.futures.size(), 5u);
+  std::size_t idx = 0;
+  for (auto& fc: res.futures)
+  {
+    ASSERT_TRUE(fc.valid());
+    EXPECT_EQ(fc.is_ready(), idx++ == res.index);
+  }
+  EXPECT_EQ(res.futures[res.index].get(), "42");
+}
+
+TEST(WhenAnyVectorTest, multiple_futures_one_initionally_ready) {
+  experimental::promise<void> ps[5];
+  experimental::future<void> fs[5];
+
+  std::transform(
+    std::begin(ps), std::end(ps),
+    std::begin(fs),
+    std::mem_fn(&experimental::promise<void>::get_future)
+  );
+  ps[1].set_value();
+  ASSERT_TRUE(fs[1].is_ready());
+
+  auto f = experimental::when_any(std::begin(fs), std::end(fs));
+  ASSERT_TRUE(f.valid());
+  ASSERT_TRUE(f.is_ready());
+
+  auto res = f.get();
+  EXPECT_EQ(res.index, 1u);
+  EXPECT_EQ(res.futures.size(), 5u);
+  std::size_t idx = 0;
+  for (auto& fc: res.futures)
+  {
+    ASSERT_TRUE(fc.valid());
+    EXPECT_EQ(fc.is_ready(), idx++ == res.index);
+  }
+  ASSERT_NO_THROW(res.futures[res.index].get());
+}
+
+TEST(WhenAnyVectorTest, multiple_shared_futures_one_initionally_ready) {
+  int some_var = 42;
+
+  experimental::promise<int&> ps[5];
+  experimental::shared_future<int&> fs[5];
+
+  std::transform(
+    std::begin(ps), std::end(ps),
+    std::begin(fs),
+    std::mem_fn(&experimental::promise<int&>::get_future)
+  );
+  ps[0].set_value(some_var);
+  ASSERT_TRUE(fs[0].is_ready());
+
+  auto f = experimental::when_any(std::begin(fs), std::end(fs));
+  ASSERT_TRUE(f.valid());
+  ASSERT_TRUE(f.is_ready());
+
+  auto res = f.get();
+  EXPECT_EQ(res.index, 0u);
+  EXPECT_EQ(res.futures.size(), 5u);
+  std::size_t idx = 0;
+  for (auto& fc: res.futures)
+  {
+    ASSERT_TRUE(fc.valid());
+    EXPECT_EQ(fc.is_ready(), idx++ == res.index);
+  }
+  EXPECT_EQ(&res.futures[res.index].get(), &some_var);
 }
 
 }
