b07b206f860529695cb66f2f0fe3e9f3be66d9c3
Sergey Vidyuk <sir.vestnik@gmail.com>
2017-07-15 00:37:27 +0700

    Some travis docker images improvements
     * CC and CXX are set by container
     * No modifications are done in source tree
     * Entrypoint is set for container to make command line shorter

--- .travis/autobuild.sh
+++ .travis/autobuild.sh
@@ -1,18 +1,15 @@
-#!/bin/sh
+#!/bin/bash -e
 
-set -e
+SRCDIR=$(dirname $(dirname $0))
 
-export CC=$1
-export CXX=$2
-export CONAN_ENV_COMPILER_LIBCXX=$3
-export BUILD_TYPE=$4
+export CONAN_ENV_COMPILER_LIBCXX=$1
+export BUILD_TYPE=$2
 
 export CONAN_CMAKE_GENERATOR=Ninja
 
-conan install -s build_type=${BUILD_TYPE} --build=missing
+conan install -s build_type=${BUILD_TYPE} --build=missing ${SRCDIR}
 
-mkdir build
-cd build
-cmake -G Ninja -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCONAN_LIBCXX=${CONAN_ENV_COMPILER_LIBCXX} ..
+cmake -G Ninja -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCONAN_LIBCXX=${CONAN_ENV_COMPILER_LIBCXX} ${SRCDIR}
 ninja
+
 ./bin/unit_tests
--- .travis/cxx-conan-gcc-5.dockerfile
+++ .travis/cxx-conan-gcc-5.dockerfile
@@ -6,3 +6,13 @@ RUN echo "deb http://mirror.yandex.ru/debian sid main" > /etc/apt/sources.list.d
     apt-get -y --no-install-recommends install gcc-5 g++-5 libstdc++-5-dev && \
     apt-get -y clean && \
     rm -rf /var/lib/apt/lists
+
+VOLUME /home/builder/src
+USER builder
+RUN mkdir /home/builder/build
+WORKDIR /home/builder/build
+ENV CC gcc-5
+ENV CXX g++-5
+
+CMD ["libstdc++11", "Release"]
+ENTRYPOINT ["/home/builder/src/.travis/autobuild.sh"]
