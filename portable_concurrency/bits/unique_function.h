3ffad9b4681e04761d7e2e8889257af392aa5263
Sergey Vidyuk <sir.vestnik@gmail.com>
2017-08-22 10:47:23 +0700

    Do not double-wrap std::function in unique_function implementation

--- portable_concurrency/bits/unique_function.h
+++ portable_concurrency/bits/unique_function.h
@@ -36,25 +36,100 @@ class unique_function;
 // http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2015/n4543.pdf
 template<typename R, typename... A>
 class unique_function<R(A...)> {
+  enum class stored_type {empty, std_function, heap_invokable};
 public:
-  unique_function() = default;
+  unique_function() noexcept {}
 
   template<typename F>
-  unique_function(F&& f): func_(std::make_unique<detail::invokable_wrapper<F, R, A...>>(std::forward<F>(f))) {}
+  unique_function(F&& f): unique_function() {
+    new (&heap_invokable_) std::unique_ptr<detail::invokable<R, A...>>{
+      std::make_unique<detail::invokable_wrapper<F, R, A...>>(std::forward<F>(f))
+    };
+    type_ = stored_type::heap_invokable;
+  }
 
-  ~unique_function() = default;
+  unique_function(std::function<R(A...)> f): unique_function() {
+    new (&std_function_) std::function<R(A...)>{std::move(f)};
+    type_ = stored_type::std_function;
+  }
 
-  unique_function(unique_function&& rhs) noexcept = default;
-  unique_function& operator= (unique_function&& rhs) noexcept = default;
+  ~unique_function() {
+    destroy_stored_object();
+  }
+
+  unique_function(unique_function&& rhs) noexcept(std::is_nothrow_move_constructible<std::function<R(A...)>>::value):
+    unique_function()
+  {
+    switch (rhs.type_) {
+    case stored_type::empty: return;
+
+    case stored_type::std_function:
+      new (&std_function_) std::function<R(A...)>{std::move(rhs.std_function_)};
+      type_ = stored_type::std_function;
+    return;
+
+    case stored_type::heap_invokable:
+      new (&heap_invokable_) std::unique_ptr<detail::invokable<R, A...>>{std::move(rhs.heap_invokable_)};
+      type_ = stored_type::heap_invokable;
+    return;
+    }
+  }
+
+  unique_function& operator= (unique_function&& rhs)
+    noexcept(std::is_nothrow_move_assignable<std::function<R(A...)>>::value)
+  {
+    if (type_ == rhs.type_) {
+      switch (type_) {
+      case stored_type::empty: break;
+      case stored_type::std_function: std_function_ = std::move(rhs.std_function_); break;
+      case stored_type::heap_invokable: heap_invokable_ = std::move(rhs.heap_invokable_); break;
+      }
+      return *this;
+    }
+
+    destroy_stored_object();
+    type_ = stored_type::empty;
+
+    switch (rhs.type_) {
+    case stored_type::empty: return *this;
+
+    case stored_type::std_function:
+      new (&std_function_) std::function<R(A...)>{std::move(rhs.std_function_)};
+      type_ = stored_type::std_function;
+    return *this;
+
+    case stored_type::heap_invokable:
+      new (&heap_invokable_) std::unique_ptr<detail::invokable<R, A...>>{std::move(rhs.heap_invokable_)};
+      type_ = stored_type::heap_invokable;
+    return *this;
+    }
+  }
 
   R operator() (A... args) {
-    if (!func_)
-      throw std::bad_function_call{};
-    return func_->invoke(args...);
+    switch (type_) {
+    case stored_type::empty: break;
+    case stored_type::std_function: return std_function_(args...);
+    case stored_type::heap_invokable: return heap_invokable_->invoke(args...);
+    }
+    throw std::bad_function_call{};
   }
 
 private:
-  std::unique_ptr<detail::invokable<R, A...>> func_;
+  void destroy_stored_object() noexcept {
+    switch (type_) {
+    case stored_type::empty: return;
+    case stored_type::std_function: std_function_.~function(); return;
+    case stored_type::heap_invokable: heap_invokable_.~unique_ptr(); return;
+    }
+  }
+
+private:
+  stored_type type_ = stored_type::empty;
+  union {
+    bool empty_;
+    std::function<R(A...)> std_function_;
+    std::unique_ptr<detail::invokable<R, A...>> heap_invokable_;
+  };
 };
 
 } // inline namespace cxx14_v1
