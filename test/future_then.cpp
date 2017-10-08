b1f4c2b2006882eb70941db4b24f834cd33bd484
Sergey Vidyuk <sir.vestnik@gmail.com>
2017-10-08 22:30:54 +0700

    Remove duplicated checks from future::then tests

--- test/future_then.cpp
+++ test/future_then.cpp
@@ -18,18 +18,12 @@ template<typename T>
 void continuation_call() {
   pc::promise<T> p;
   auto f = p.get_future();
-  ASSERT_TRUE(f.valid());
 
   pc::future<std::string> string_f = f.then([](pc::future<T>&& ready_f) {
-    EXPECT_TRUE(ready_f.is_ready());
     return to_string(ready_f.get());
   });
-  EXPECT_FALSE(f.valid());
-  EXPECT_TRUE(string_f.valid());
-  EXPECT_FALSE(string_f.is_ready());
 
-  p.set_value(some_value<T>());
-  EXPECT_TRUE(string_f.is_ready());
+  set_promise_value(p);
   EXPECT_EQ(string_f.get(), to_string(some_value<T>()));
 }
 
@@ -37,52 +31,34 @@ template<>
 void continuation_call<void>() {
   pc::promise<void> p;
   auto f = p.get_future();
-  ASSERT_TRUE(f.valid());
 
   pc::future<std::string> string_f = f.then([](pc::future<void>&& ready_f) {
-    EXPECT_TRUE(ready_f.is_ready());
     ready_f.get();
     return "void value"s;
   });
-  EXPECT_FALSE(f.valid());
-  EXPECT_TRUE(string_f.valid());
-  EXPECT_FALSE(string_f.is_ready());
 
-  p.set_value();
-  EXPECT_TRUE(string_f.is_ready());
+  set_promise_value(p);
   EXPECT_EQ(string_f.get(), "void value"s);
 }
 
 template<typename T>
 void async_continuation_call() {
   auto f = set_value_in_other_thread<T>(25ms);
-  ASSERT_TRUE(f.valid());
 
   pc::future<std::string> string_f = f.then([](pc::future<T>&& ready_f) {
-    EXPECT_TRUE(ready_f.is_ready());
     return to_string(ready_f.get());
   });
-  EXPECT_FALSE(f.valid());
-  EXPECT_TRUE(string_f.valid());
-  EXPECT_FALSE(string_f.is_ready());
-
   EXPECT_EQ(string_f.get(), to_string(some_value<T>()));
 }
 
 template<>
 void async_continuation_call<void>() {
   auto f = set_value_in_other_thread<void>(25ms);
-  ASSERT_TRUE(f.valid());
 
   pc::future<std::string> string_f = f.then([](pc::future<void>&& ready_f) {
-    EXPECT_TRUE(ready_f.is_ready());
     ready_f.get();
     return "void value"s;
   });
-  EXPECT_FALSE(f.valid());
-  EXPECT_TRUE(string_f.valid());
-  EXPECT_FALSE(string_f.is_ready());
-
   EXPECT_EQ(string_f.get(), "void value"s);
 }
 
@@ -91,13 +67,8 @@ void ready_continuation_call() {
   auto f = pc::make_ready_future(some_value<T>());
 
   pc::future<std::string> string_f = f.then([](pc::future<T>&& ready_f) {
-    EXPECT_TRUE(ready_f.is_ready());
     return to_string(ready_f.get());
   });
-  EXPECT_FALSE(f.valid());
-  EXPECT_TRUE(string_f.valid());
-  EXPECT_TRUE(string_f.is_ready());
-
   EXPECT_EQ(string_f.get(), to_string(some_value<T>()));
 }
 
@@ -106,13 +77,8 @@ void ready_continuation_call<future_tests_env&>() {
   auto f = pc::make_ready_future(std::ref(some_value<future_tests_env&>()));
 
   pc::future<std::string> string_f = f.then([](pc::future<future_tests_env&>&& ready_f) {
-    EXPECT_TRUE(ready_f.is_ready());
     return to_string(ready_f.get());
   });
-  EXPECT_FALSE(f.valid());
-  EXPECT_TRUE(string_f.valid());
-  EXPECT_TRUE(string_f.is_ready());
-
   EXPECT_EQ(string_f.get(), to_string(some_value<future_tests_env&>()));
 }
 
@@ -122,13 +88,9 @@ void void_continuation() {
   bool executed = false;
 
   pc::future<void> void_f = f.then([&executed](pc::future<T>&& ready_f) -> void {
-    EXPECT_TRUE(ready_f.is_ready());
     ready_f.get();
     executed = true;
   });
-  EXPECT_FALSE(f.valid());
-  EXPECT_TRUE(void_f.valid());
-  EXPECT_FALSE(void_f.is_ready());
 
   EXPECT_NO_THROW(void_f.get());
   EXPECT_TRUE(executed);
@@ -140,14 +102,9 @@ void ready_void_continuation() {
   bool executed = false;
 
   pc::future<void> void_f = f.then([&executed](pc::future<T>&& ready_f) -> void {
-    EXPECT_TRUE(ready_f.is_ready());
     ready_f.get();
     executed = true;
   });
-  EXPECT_FALSE(f.valid());
-  EXPECT_TRUE(void_f.valid());
-  EXPECT_TRUE(void_f.is_ready());
-
   EXPECT_NO_THROW(void_f.get());
   EXPECT_TRUE(executed);
 }
@@ -158,13 +115,9 @@ void ready_void_continuation<future_tests_env&>() {
   bool executed = false;
 
   pc::future<void> void_f = f.then([&executed](pc::future<future_tests_env&>&& ready_f) -> void {
-    EXPECT_TRUE(ready_f.is_ready());
     ready_f.get();
     executed = true;
   });
-  EXPECT_FALSE(f.valid());
-  EXPECT_TRUE(void_f.valid());
-  EXPECT_TRUE(void_f.is_ready());
 
   EXPECT_NO_THROW(void_f.get());
   EXPECT_TRUE(executed);
@@ -176,13 +129,9 @@ void ready_void_continuation<void>() {
   bool executed = false;
 
   pc::future<void> void_f = f.then([&executed](pc::future<void>&& ready_f) -> void {
-    EXPECT_TRUE(ready_f.is_ready());
     ready_f.get();
     executed = true;
   });
-  EXPECT_FALSE(f.valid());
-  EXPECT_TRUE(void_f.valid());
-  EXPECT_TRUE(void_f.is_ready());
 
   EXPECT_NO_THROW(void_f.get());
   EXPECT_TRUE(executed);
@@ -193,13 +142,9 @@ void ready_continuation_call<void>() {
   auto f = pc::make_ready_future();
 
   pc::future<std::string> string_f = f.then([](pc::future<void>&& ready_f) {
-    EXPECT_TRUE(ready_f.is_ready());
     ready_f.get();
     return "void value"s;
   });
-  EXPECT_FALSE(f.valid());
-  EXPECT_TRUE(string_f.valid());
-  EXPECT_TRUE(string_f.is_ready());
 
   EXPECT_EQ(string_f.get(), "void value"s);
 }
@@ -209,13 +154,9 @@ void exception_to_continuation() {
   auto f = set_error_in_other_thread<T>(25ms, std::runtime_error("test error"));
 
   pc::future<std::string> string_f = f.then([](pc::future<T>&& ready_f) {
-    EXPECT_TRUE(ready_f.is_ready());
     EXPECT_RUNTIME_ERROR(ready_f, "test error");
     return "Exception delivered"s;
   });
-  EXPECT_FALSE(f.valid());
-  EXPECT_TRUE(string_f.valid());
-  EXPECT_FALSE(string_f.is_ready());
 
   EXPECT_EQ(string_f.get(), "Exception delivered"s);
 }
@@ -225,13 +166,9 @@ void exception_to_ready_continuation() {
   auto f = pc::make_exceptional_future<T>(std::runtime_error("test error"));
 
   pc::future<std::string> string_f = f.then([](pc::future<T>&& ready_f) {
-    EXPECT_TRUE(ready_f.is_ready());
     EXPECT_RUNTIME_ERROR(ready_f, "test error");
     return "Exception delivered"s;
   });
-  EXPECT_FALSE(f.valid());
-  EXPECT_TRUE(string_f.valid());
-  EXPECT_TRUE(string_f.is_ready());
 
   EXPECT_EQ(string_f.get(), "Exception delivered"s);
 }
