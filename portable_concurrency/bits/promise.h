54f848d0ee7cf6d64cfa6f39e48c34629da3c2a4
Sergey Vidyuk <sir.vestnik@gmail.com>
2017-10-25 23:05:30 +0700

    Switch promise to use either for cancelation

--- portable_concurrency/bits/promise.h
+++ portable_concurrency/bits/promise.h
@@ -3,6 +3,7 @@
 #include <memory>
 #include <utility>
 
+#include "either.h"
 #include "future.h"
 #include "shared_state.h"
 #include "utils.h"
@@ -12,58 +13,52 @@ inline namespace cxx14_v1 {
 namespace detail {
 
 template<typename T>
-class promise_base {
-protected:
-  std::weak_ptr<shared_state<T>> state_;
-  std::shared_ptr<future_state<T>> future_state_;
+struct promise_common {
+  either<std::shared_ptr<shared_state<T>>, std::weak_ptr<shared_state<T>>> state_{
+    first_t{}, std::make_shared<shared_state<T>>()
+  };
 
-  void check_state() {
-    // the only way to distinguish empty weak_ptr from expired is owner_before method
-    std::weak_ptr<shared_state<T>> empty{};
-    if (!empty.owner_before(state_) && !state_.owner_before(empty))
-      throw std::future_error(std::future_errc::no_state);
-  }
-
-  std::shared_ptr<shared_state<T>> get_state() {
-    auto state = state_.lock();
-    if (!state)
-      check_state();
-    return state;
-  }
-
-public:
-  promise_base() {
-    auto state = std::make_shared<shared_state<T>>();
-    future_state_ = state;
-    state_ = std::move(state);
-  }
-  ~promise_base() {
-    auto state = state_.lock();
+  promise_common() = default;
+  ~promise_common() {
+    auto state = get_state(false);
     if (state && !state->continuations().executed())
       state->set_exception(std::make_exception_ptr(std::future_error{std::future_errc::broken_promise}));
   }
 
-  promise_base(promise_base&&) noexcept = default;
-  promise_base& operator= (promise_base&&) noexcept = default;
+  promise_common(promise_common&&) noexcept = default;
+  promise_common& operator= (promise_common&&) noexcept = default;
 
   future<T> get_future() {
-    if (!future_state_)
-      check_state(), throw std::future_error(std::future_errc::future_already_retrieved);
-    return {std::move(future_state_)};
+    if (state_.state() == either_state::second)
+      throw std::future_error(std::future_errc::future_already_retrieved);
+    auto state = get_state();
+    state_.emplace(second_t{}, state);
+    return {state};
   }
 
   void set_exception(std::exception_ptr error) {
-    auto state = state_.lock();
-    if (!state)
-      return check_state();
-    state->set_exception(error);
+    auto state = get_state();
+    if (state)
+      state->set_exception(error);
+  }
+
+  std::shared_ptr<shared_state<T>> get_state(bool throw_no_state = true) {
+    switch (state_.state())
+    {
+    case either_state::first: return state_.get(first_t{});
+    case either_state::second: return state_.get(second_t{}).lock();
+    case either_state::empty: break;
+    }
+    if (throw_no_state)
+      throw std::future_error(std::future_errc::no_state);
+    return nullptr;
   }
 };
 
 } // namespace detail
 
 template<typename T>
-class promise: public detail::promise_base<T> {
+class promise {
 public:
   promise() = default;
   promise(promise&&) noexcept = default;
@@ -75,20 +70,26 @@ public:
   ~promise() = default;
 
   void set_value(const T& val) {
-    auto state = this->get_state();
+    auto state = common_.get_state();
     if (state)
       state->emplace(val);
   }
 
   void set_value(T&& val) {
-    auto state = this->get_state();
+    auto state = common_.get_state();
     if (state)
       state->emplace(std::move(val));
   }
+
+  future<T> get_future() {return common_.get_future();}
+  void set_exception(std::exception_ptr error) {common_.set_exception(error);}
+
+private:
+  detail::promise_common<T> common_;
 };
 
 template<typename T>
-class promise<T&>: public detail::promise_base<T&> {
+class promise<T&> {
 public:
   promise() = default;
   promise(promise&&) noexcept = default;
@@ -100,14 +101,20 @@ public:
   ~promise() = default;
 
   void set_value(T& val) {
-    auto state = this->get_state();
+    auto state = common_.get_state();
     if (state)
       state->emplace(val);
   }
+
+  future<T&> get_future() {return common_.get_future();}
+  void set_exception(std::exception_ptr error) {common_.set_exception(error);}
+
+private:
+  detail::promise_common<T&> common_;
 };
 
 template<>
-class promise<void>: public detail::promise_base<void> {
+class promise<void> {
 public:
   promise() = default;
   promise(promise&&) noexcept = default;
@@ -119,10 +126,16 @@ public:
   ~promise() = default;
 
   void set_value()  {
-    auto state = this->get_state();
+    auto state = common_.get_state();
     if (state)
       state->emplace();
   }
+
+  future<void> get_future() {return common_.get_future();}
+  void set_exception(std::exception_ptr error) {common_.set_exception(error);}
+
+private:
+  detail::promise_common<void> common_;
 };
 
 } // inline namespace cxx14_v1
