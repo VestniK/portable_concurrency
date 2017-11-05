f22d654259f1ca7c2d0192c1083c7152d7e9ff1e
Sergey Vidyuk <sir.vestnik@gmail.com>
2017-11-05 11:49:39 +0600

    Async tests and bug fixes

--- portable_concurrency/bits/async.h
+++ portable_concurrency/bits/async.h
@@ -10,10 +10,10 @@ inline namespace cxx14_v1 {
 
 template<typename E, typename F, typename... A>
 auto async(E&& exec, F&& func, A&&... a) -> std::enable_if_t<
-  is_executor<E>::value,
+  is_executor<std::decay_t<E>>::value,
   detail::add_future_t<std::result_of_t<F(A...)>>
 > {
-  // TODO: provide better implementation needed
+  // TODO: provide better implementation
   return make_ready_future().next(
     std::forward<E>(exec),
     std::bind(std::forward<F>(func), std::forward<F>(a)...)
---
+++ test/async.cpp
@@ -0,0 +1,50 @@
+#include <gtest/gtest.h>
+
+#include <portable_concurrency/future>
+
+#include "test_tools.h"
+
+namespace portable_concurrency {
+namespace {
+namespace test {
+
+struct Async: future_test {};
+
+TEST_F(Async, returns_valid_future) {
+  pc::future<int> future = pc::async(g_future_tests_env, [] {return 42;});
+  EXPECT_TRUE(future.valid());
+}
+
+TEST_F(Async, delivers_function_result) {
+  pc::future<int> future = pc::async(g_future_tests_env, [] {return 42;});
+  EXPECT_EQ(future.get(), 42);
+}
+
+TEST_F(Async, executes_functor_on_specified_executor) {
+  pc::future<std::thread::id> future = pc::async(g_future_tests_env, [] {return std::this_thread::get_id();});
+  EXPECT_TRUE(g_future_tests_env->uses_thread(future.get()));
+}
+
+TEST_F(Async, support_abandon_operation) {
+  pc::future<void> future = pc::async(null_executor, [] {});
+  EXPECT_FUTURE_ERROR(future.get(), std::future_errc::broken_promise);
+}
+
+TEST_F(Async, unwraps_future) {
+  pc::future<int> future = pc::async(g_future_tests_env, [] {
+    return pc::async(g_future_tests_env, [] {return 100500;});
+  });
+  EXPECT_EQ(future.get(), 100500);
+}
+
+TEST_F(Async, unwraps_shared_future) {
+  auto future = pc::async(g_future_tests_env, [] {
+    return pc::async(g_future_tests_env, [] {return 100500;}).share();
+  });
+  static_assert (std::is_same<decltype(future), pc::shared_future<int>>::value, "");
+  EXPECT_EQ(future.get(), 100500);
+}
+
+} // namespace test
+} // anonymous namespace
+} // namespace portable_conurrency
