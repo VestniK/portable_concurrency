6d685aa904a11e02e598cc529df4119afd5fff49
Sergey Vidyuk <sir.vestnik@gmail.com>
2018-05-17 01:30:04 +0700

    Conan package recipe

---
+++ conanfile.py
@@ -0,0 +1,28 @@
+from conans import ConanFile, CMake
+
+
+class PortableconCurrencyConan(ConanFile):
+    build_requires = "gtest/1.8.0@VestniK/stable"
+    name = "portable_concurrency"
+    version = "0.7.0"
+    license = "CC0"
+    url = "https://github.com/VestniK/portable_concurrency"
+    description = "Portable future/promise implemenattion close to ConcurrencyTS specification"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
+    generators = "cmake"
+    exports_sources = "*", "!build/*"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="./")
+        cmake.build()
+        cmake.test()
+        cmake.install()
+
+    def package(self):
+        pass
+
+    def package_info(self):
+        self.cpp_info.libs = ["portable_concurrency"]
