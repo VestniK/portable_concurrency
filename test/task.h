74b7ad395820e9ca2abf15aa72639c7212131879
Sergey Vidyuk <sir.vestnik@gmail.com>
2018-05-31 11:21:05 +0700

    Simplify test thread pool task code

--- test/task.h
+++ test/task.h
@@ -7,26 +7,21 @@
 #include <portable_concurrency/bits/invoke.h>
 
 template<typename F, typename... A>
-class task {
-public:
-  task(F&& f, A&&... a):
-    func_(std::forward<F>(f)),
-    args_(std::forward<A>(a)...)
-  {}
-
+struct task {
   auto operator() () {
     return run(std::make_index_sequence<sizeof...(A)>());
   }
 
-private:
   template<size_t... I>
   auto run(std::index_sequence<I...>) {
     return pc::detail::invoke(std::move(func_), std::move(std::get<I>(args_))...);
   }
 
-  std::decay_t<F> func_;
-  std::tuple<std::decay_t<A>...> args_;
+  F func_;
+  std::tuple<A...> args_;
 };
 
 template<typename F, typename... A>
-task<F, A...> make_task(F&& f, A&&... a) {return {std::forward<F>(f), std::forward<A>(a)...};}
+task<std::decay_t<F>, std::decay_t<A>...> make_task(F&& f, A&&... a) {
+  return {std::forward<F>(f), std::forward_as_tuple(std::forward<A>(a)...)};
+}
