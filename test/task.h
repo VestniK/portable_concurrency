f08feae03d37d129fe4b0f8db44377e1245bcf35
Sergey Vidyuk <sir.vestnik@gmail.com>
2018-10-12 23:44:09 +0700

    Simplify task class

--- test/task.h
+++ test/task.h
@@ -4,29 +4,25 @@
 #include <tuple>
 #include <utility>
 
+#include <portable_concurrency/bits/alias_namespace.h>
 #include <portable_concurrency/bits/invoke.h>
 
 template<typename F, typename... A>
-class task {
-public:
-  task(F&& f, A&&... a):
-    func_(std::forward<F>(f)),
-    args_(std::forward<A>(a)...)
-  {}
-
+struct task {
   auto operator() () {
     return run(std::make_index_sequence<sizeof...(A)>());
   }
 
-private:
   template<size_t... I>
   auto run(std::index_sequence<I...>) {
-    return pc::detail::invoke(std::move(func_), std::move(std::get<I>(args_))...);
+    return pc::detail::invoke(std::move(func), std::move(std::get<I>(args))...);
   }
 
-  std::decay_t<F> func_;
-  std::tuple<std::decay_t<A>...> args_;
+  F func;
+  std::tuple<A...> args;
 };
 
 template<typename F, typename... A>
-task<F, A...> make_task(F&& f, A&&... a) {return {std::forward<F>(f), std::forward<A>(a)...};}
+task<std::decay_t<F>, std::decay_t<A>...> make_task(F&& f, A&&... a) {
+  return {std::forward<F>(f), std::forward_as_tuple(std::forward<A>(a)...)};
+}
