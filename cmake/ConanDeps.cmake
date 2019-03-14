3db901822123095b531c4da52c822a393917d221
Sergey Vidyuk <sir.vestnik@gmail.com>
2019-03-14 14:32:04 +0700

    Avoid using CMAKE_BINARY_DIR
    This variable can point to unpredictable location if portable_cuncurrency
    is included in to cinsumer sources via add_subdirectory

--- cmake/ConanDeps.cmake
+++ cmake/ConanDeps.cmake
@@ -1,5 +1,5 @@
-if (EXISTS "${CMAKE_BINARY_DIR}/conanbuildinfo.cmake")
-  include("${CMAKE_BINARY_DIR}/conanbuildinfo.cmake")
+if (EXISTS "${portable_concurrency_BINARY_DIR}/conanbuildinfo.cmake")
+  include("${portable_concurrency_BINARY_DIR}/conanbuildinfo.cmake")
   set(CONAN_CMAKE_FIND_ROOT_PATH ${CONAN_CMAKE_MODULE_PATH})
   conan_basic_setup(TARGETS)
 endif()
