51599740348af2cf7058a8d1959f18af5e1485db
Sergey Vidyuk <sir.vestnik@gmail.com>
2019-03-30 00:32:35 +0700

    Documentation improvements

--- portable_concurrency/bits/timed_waiter.h
+++ portable_concurrency/bits/timed_waiter.h
@@ -11,6 +11,14 @@
 namespace portable_concurrency {
 inline namespace cxx14_v1 {
 
+/**
+ * @ingroup timed_waiter_hdr
+ * @brief Class to wait on @ref future or `shared_future` with timeout
+ *
+ * This class is intended to become complete replacement for `wait_for` and `wait_until` member functions of @ref
+ * future and `shared_future` classes. Both of those functions are rarely used but prevent internal implementation
+ * optimizations.
+ */
 class timed_waiter {
 private:
   struct waiter {
@@ -20,8 +28,16 @@ private:
   };
 
 public:
+  /**
+   * @brief Constructs `timed_waiter` in some unspecified state.
+   *
+   * No operations are allowed on the default constructed object except copy/move asignment and destruction.
+   */
   timed_waiter() noexcept = default;
 
+  /**
+   * @brief Constructs `timed_waiter` associated with @a fut object.
+   */
   template <typename T>
   explicit timed_waiter(future<T>& fut) : waiter_{std::make_shared<waiter>()} {
     fut.notify([waiter = waiter_] {
@@ -29,6 +45,9 @@ public:
       waiter->cv.notify_all();
     });
   }
+  /**
+   * @brief Constructs `timed_waiter` associated with @a fut object.
+   */
   template <typename T>
   explicit timed_waiter(shared_future<T>& fut) : waiter_{std::make_shared<waiter>()} {
     fut.notify([waiter = waiter_] {
@@ -37,6 +56,9 @@ public:
     });
   }
 
+  /**
+   * @brief Waits for the result, returns if it is not available for the specified timeout duration.
+   */
   template <typename Rep, typename Per>
   future_status wait_for(const std::chrono::duration<Rep, Per>& dur) {
     std::unique_lock<std::mutex> lk{waiter_->mutex};
@@ -44,6 +66,9 @@ public:
                                                                             : future_status::timeout;
   }
 
+  /**
+   * @brief Waits for the result, returns if it is not available until specified time point has been reached.
+   */
   template <typename Clock, typename Dur>
   future_status wait_until(const std::chrono::time_point<Clock, Dur>& tp) {
     std::unique_lock<std::mutex> lk{waiter_->mutex};
