aca259b965f2dc3544cbec6c5db16b764a20eb20
Sergey Vidyuk <sir.vestnik@gmail.com>
2019-06-21 20:53:58 +0700

    future_get adpter to simplify work with std algorithms

---
+++ portable_concurrency/bits/algo_adapters.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <portable_concurrency/bits/future.h>
+#include <portable_concurrency/bits/shared_future.h>
+
+namespace portable_concurrency {
+inline namespace cxx14_v1 {
+
+struct future_get_t {
+  template <typename T>
+  decltype(auto) operator()(future<T>& f) const {
+    return f.get();
+  }
+
+  template <typename T>
+  decltype(auto) operator()(const shared_future<T>& f) const {
+    return f.get();
+  }
+};
+
+/**
+ * @headerfile portable_concurrency/future
+ * @ingroup future_hdr
+ * @brief Unary operation which takes a future and returns value stored inside it
+ *
+ * The main use case for this function object is to transform collection of futures into collection of values. For
+ * example:
+ * ```
+ * pc::future<std::vector<int>> = pc::when_all(run_many_tasks())
+ *   .next([](std::vector<pc::future<int>> futures){
+ *     std::vector<int> results;
+ *     results.reserve(futuers.size());
+ *     std::transform(futures.begin(), futures.end(), std::back_inserter(results), pc::future_get);
+ *     return results;
+ *   })
+ * ```
+ */
+constexpr future_get_t future_get;
+
+} // namespace cxx14_v1
+} // namespace portable_concurrency
---
+++ test/algo_adapters.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <vector>
+
+#include <gtest/gtest.h>
+
+#include <portable_concurrency/future>
+
+namespace {
+
+template <template <class...> typename Future>
+std::vector<Future<int>> make_test_futures(std::initializer_list<int> values) {
+  std::vector<Future<int>> futures;
+  for (int val : values)
+    futures.push_back(pc::make_ready_future(val));
+  return futures;
+}
+
+TEST(future_get_transformation, extract_values_from_futures) {
+  std::vector<pc::future<int>> futures = make_test_futures<pc::future>({100, 500, 42, 0xff});
+
+  std::vector<int> values;
+  values.reserve(futures.size());
+  std::transform(futures.begin(), futures.end(), std::back_inserter(values), pc::future_get);
+  EXPECT_EQ(values, (std::vector<int>{100, 500, 42, 0xff}));
+}
+
+TEST(future_get_transformation, extract_values_from_shared_futures) {
+  std::vector<pc::shared_future<int>> futures = make_test_futures<pc::shared_future>({100, 500, 42, 0xff});
+
+  std::vector<int> values;
+  values.reserve(futures.size());
+  std::transform(futures.begin(), futures.end(), std::back_inserter(values), pc::future_get);
+  EXPECT_EQ(values, (std::vector<int>{100, 500, 42, 0xff}));
+}
+
+} // namespace
