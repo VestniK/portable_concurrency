929d5ed17c74f4a0a6ec66b9f9a99adfd6c27b10
Sergey Vidyuk <sir.vestnik@gmail.com>
2019-09-01 13:08:49 +0700

    when_all documentation

--- portable_concurrency/bits/when_all.h
+++ portable_concurrency/bits/when_all.h
@@ -60,13 +60,44 @@ private:
 
 future<std::tuple<>> when_all();
 
+#ifdef DOXYGEN
+/**
+ * @ingroup future_hdr
+ *
+ * Create a future object that becomes ready when all of the input futures and shared_futures become ready. The behavior
+ * is undefined if any input future or shared_future is invalid. Ready `future<std::tuple<>>` immediatelly returned if
+ * input futures is empty.
+ *
+ * This function template participates in overload resolution only if all of the arguments are either `future<T>` or
+ * `shared_future<T>`.
+ */
+template <typename... Futures>
+future<std::tuple<Futures...>> when_all(Futures&&...);
+#else
 template <typename... Futures>
 auto when_all(Futures&&... futures) -> std::enable_if_t<detail::are_futures<std::decay_t<Futures>...>::value,
     future<std::tuple<std::decay_t<Futures>...>>> {
   using Sequence = std::tuple<std::decay_t<Futures>...>;
   return {detail::when_all_state<Sequence>::make(Sequence{std::forward<Futures>(futures)...})};
 }
+#endif
 
+#ifdef DOXYGEN
+/**
+ * @ingroup future_hdr
+ *
+ * Create a future object that becomes ready when all of the input futures and shared_futures become ready. The
+ * behavior is undefined if any input future or shared_future is invalid. Ready `future<std::tuple<>>` immediatelly
+ * returned if input futures is empty range (`first == last`). Every input `future<T>` object is moved into
+ * corresponding object of the returned future shared state, and every `shared_future<T>` object is copied. The order of
+ * the objects in the returned `future` object matches the order of arguments.
+ *
+ * This function template participates in overload resolution only if value type of the InputIt is either `future<T>` or
+ * `shared_future<T>`.
+ */
+template <typename InputIt>
+future<std::vector<typename std::iterator_traits<InputIt>::value_type>> when_all(InputIt first, InputIt last);
+#else
 template <typename InputIt>
 auto when_all(InputIt first, InputIt last)
     -> std::enable_if_t<detail::is_unique_future<typename std::iterator_traits<InputIt>::value_type>::value,
@@ -87,6 +118,7 @@ auto when_all(InputIt first, InputIt last)
     return make_ready_future(Sequence{});
   return {detail::when_all_state<Sequence>::make(Sequence{first, last})};
 }
+#endif
 
 } // namespace cxx14_v1
 } // namespace portable_concurrency
