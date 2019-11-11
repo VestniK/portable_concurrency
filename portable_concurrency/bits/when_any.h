a0ab372dd73b3c868c92f24722cdd0790ee06f6e
Sergey Vidyuk <sir.vestnik@gmail.com>
2019-11-11 18:19:36 +0700

    when_any function documentation

--- portable_concurrency/bits/when_any.h
+++ portable_concurrency/bits/when_any.h
@@ -16,9 +16,17 @@
 namespace portable_concurrency {
 inline namespace cxx14_v1 {
 
+/*!
+ * @ingroup future_hdr
+ *
+ * Structure hoding sequence of futures passed as arguments to one of the `when_any` function overload and the index of
+ * a future in this sequence which is ready.
+ */
 template <typename Sequence>
 struct when_any_result {
+  //! Index of a ready future in the `futures` field.
   std::size_t index;
+  //! Sequence of futures passed as an argument to `when_any` function. Input order is always preserved.
   Sequence futures;
 };
 
@@ -70,6 +78,24 @@ private:
 
 future<when_any_result<std::tuple<>>> when_any();
 
+/**
+ * @ingroup future_hdr
+ *
+ * Create a future object that becomes ready when at least one of the input futures and shared_futures become ready. The
+ * behavior is undefined if any input future or shared_future is invalid. Ready `future<when_any_result<std::tuple<>>>`
+ * immediatelly returned if input futures is empty and the `index` field is `size_t(-1)`.
+ *
+ * The returned future is always `valid()`, and it becomes ready when at least one of the input futures and
+ * shared_futures the call are ready. The `index member` of the `when_any_result` contains the position of the ready
+ * future or shared_future in the `futures` member.
+ *
+ * This function template participates in overload resolution only if all of the arguments are either `future<T>` or
+ * `shared_future<T>`.
+ */
+#ifdef DOXYGEN
+template <typename... Futures>
+future<when_any_result<std::tuple<Futures...>>> when_any(Futures&&...);
+#else
 template <typename... Futures>
 auto when_any(Futures&&... futures) -> std::enable_if_t<detail::are_futures<std::decay_t<Futures>...>::value,
     future<when_any_result<std::tuple<std::decay_t<Futures>...>>>> {
@@ -77,7 +103,31 @@ auto when_any(Futures&&... futures) -> std::enable_if_t<detail::are_futures<std:
   return future<when_any_result<Sequence>>{
       detail::when_any_state<Sequence>::make(Sequence{std::forward<Futures>(futures)...})};
 }
+#endif
 
+/**
+ * @ingroup future_hdr
+ *
+ * Create a future object that becomes ready when at least one of the input futures and shared_futures become ready. The
+ * behavior is undefined if any input future or shared_future is invalid. Ready future immediatelly returned if input
+ * futures is empty range (`first == last`) and the `index` field is `size_t(-1)`. Every input `future<T>` object is
+ * moved into corresponding object of the returned future shared state, and every `shared_future<T>` object is copied.
+ * The order of the objects in the returned `future` object matches the order of arguments.
+ *
+ * The returned future is always `valid()`, and it becomes ready when at least one of the input futures and
+ * shared_futures the call are ready. The `index member` of the `when_any_result` contains the position of the ready
+ * future or shared_future in the `futures` member.
+ *
+ *
+ *
+ * This function template participates in overload resolution only if value type of the InputIt is either `future<T>` or
+ * `shared_future<T>`.
+ */
+#ifdef DOXYGEN
+template <typename InputIt>
+    future<when_any_result<std::vector<typename std::iterator_traits<InputIt>::value_type>>>>
+    when_any(InputIt first, InputIt last);
+#else
 template <typename InputIt>
 auto when_any(InputIt first, InputIt last)
     -> std::enable_if_t<detail::is_unique_future<typename std::iterator_traits<InputIt>::value_type>::value,
@@ -86,6 +136,7 @@ auto when_any(InputIt first, InputIt last)
   return future<when_any_result<Sequence>>{
       detail::when_any_state<Sequence>::make(Sequence{std::make_move_iterator(first), std::make_move_iterator(last)})};
 }
+#endif
 
 template <typename InputIt>
 auto when_any(InputIt first, InputIt last)
@@ -95,12 +146,28 @@ auto when_any(InputIt first, InputIt last)
   return future<when_any_result<Sequence>>{detail::when_any_state<Sequence>::make(Sequence{first, last})};
 }
 
+#ifdef DOXYGEN
+/**
+ * @ingroup future_hdr
+ *
+ * Create a future object that becomes ready when at least one of the input futures and shared_futures become ready. The
+ * behavior is undefined if any input future or shared_future is invalid. Effectively equivalent to
+ * `when_any(futures.begin(), futures.end())` but this overload reuses vector passed as argument instead of making new
+ * one saving one extra allocation, and supports vectors with user provided allocators.
+ *
+ * This function template participates in overload resolution only if `Future` is either `future<T>` or
+ * `shared_future<T>`.
+ */
+template <typename Future, typename Alloc>
+future<when_any_result<std::vector<Future, Alloc>>> when_any(std::vector<Future, Alloc> futures);
+#else
 template <typename Future, typename Alloc>
 auto when_any(std::vector<Future, Alloc> futures)
     -> std::enable_if_t<detail::is_future<Future>::value, future<when_any_result<std::vector<Future, Alloc>>>> {
   using Sequence = std::vector<Future, Alloc>;
   return {detail::when_any_state<Sequence>::make(std::move(futures))};
 }
+#endif
 
 } // namespace cxx14_v1
 } // namespace portable_concurrency
