e7fa90069494c076cd0ed40f294337bfabdb9fd7
Sergey Vidyuk <sir.vestnik@gmail.com>
2023-05-24 23:13:42 +0700

    Renew deps
     * Conan 2.x compatibility
     * Bump cmake requirements to 3.15
     * Update gtest to 1.13
     * Fix deprecated API usage in examples

--- conanfile.py
+++ conanfile.py
@@ -1,8 +1,10 @@
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
+from conan.tools.build import cross_building
 
 
 class PortableconCurrencyConan(ConanFile):
-    build_requires = "gtest/1.10.0"
+    build_requires = "gtest/1.13.0"
     name = "portable_concurrency"
     version = "0.11.0"
     license = "CC0"
@@ -13,8 +15,14 @@ class PortableconCurrencyConan(ConanFile):
                "no_deprecated": [True, False]}
     default_options = {"shared": False,
                        "no_deprecated": False}
-    generators = "cmake"
     exports_sources = "*", "!build/*"
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
 
     def build(self):
         cmake = CMake(self)
@@ -24,7 +32,7 @@ class PortableconCurrencyConan(ConanFile):
             cmake.definitions['PC_NO_DEPRECATED'] = True
         cmake.configure(source_folder="./")
         cmake.build()
-        if not tools.cross_building(self.settings):
+        if not cross_building(self.settings):
             cmake.test()
         cmake.install()
 
--- examples/asio/adapters.h
+++ examples/asio/adapters.h
@@ -10,43 +10,46 @@
 struct use_future_t {};
 constexpr use_future_t use_future{};
 
-template <typename R>
-struct promise_setter {
+template <typename R> struct promise_setter {
   promise_setter(use_future_t) {}
 
-  pc::future<R> get_future() { return promise_.get_future(); }
+  pc::future<R> get_future() { return std::move(promise_future_.second); }
 
-  void operator()(std::error_code ec, const R& val) {
+  void operator()(std::error_code ec, const R &val) {
     if (ec)
-      promise_.set_exception(std::make_exception_ptr(std::system_error{ec}));
+      promise_future_.first.set_exception(
+          std::make_exception_ptr(std::system_error{ec}));
     else
-      promise_.set_value(val);
+      promise_future_.first.set_value(val);
   }
 
-  void operator()(std::error_code ec, R&& val) {
+  void operator()(std::error_code ec, R &&val) {
     if (ec)
-      promise_.set_exception(std::make_exception_ptr(std::system_error{ec}));
+      promise_future_.first.set_exception(
+          std::make_exception_ptr(std::system_error{ec}));
     else
-      promise_.set_value(std::move(val));
+      promise_future_.first.set_value(std::move(val));
   }
 
-  pc::promise<R> promise_;
+  std::pair<pc::promise<R>, pc::future<R>> promise_future_ =
+      pc::make_promise<R>();
 };
 
-template <>
-struct promise_setter<void> {
+template <> struct promise_setter<void> {
   promise_setter(use_future_t) {}
 
-  pc::future<void> get_future() { return promise_.get_future(); }
+  pc::future<void> get_future() { return std::move(promise_future_.second); }
 
   void operator()(std::error_code ec) {
     if (ec)
-      promise_.set_exception(std::make_exception_ptr(std::system_error{ec}));
+      promise_future_.first.set_exception(
+          std::make_exception_ptr(std::system_error{ec}));
     else
-      promise_.set_value();
+      promise_future_.first.set_value();
   }
 
-  pc::promise<void> promise_;
+  std::pair<pc::promise<void>, pc::future<void>> promise_future_ =
+      pc::make_promise<void>();
 };
 
 namespace asio {
@@ -57,20 +60,19 @@ public:
   using completion_handler_type = ::promise_setter<R>;
   using return_type = pc::future<R>;
 
-  explicit async_result(completion_handler_type& h) : res_(h.get_future()) {}
+  explicit async_result(completion_handler_type &h) : res_(h.get_future()) {}
   return_type get() { return std::move(res_); }
 
 private:
   return_type res_;
 };
 
-template <>
-class async_result<::use_future_t, void(std::error_code)> {
+template <> class async_result<::use_future_t, void(std::error_code)> {
 public:
   using completion_handler_type = ::promise_setter<void>;
   using return_type = pc::future<void>;
 
-  explicit async_result(completion_handler_type& h) : res_(h.get_future()) {}
+  explicit async_result(completion_handler_type &h) : res_(h.get_future()) {}
   return_type get() { return std::move(res_); }
 
 private:
