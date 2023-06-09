c2b427c2ab8f63f212d13929e21b5c9ba24cb703
Vladimir Bayrashevskiy <umogSlayer@users.noreply.github.com>
2023-06-09 16:56:58 +0700

    Adapt to Conan V2

--- conanfile.py
+++ conanfile.py
@@ -4,21 +4,34 @@ from conan.tools.build import cross_building
 
 
 class PortableconCurrencyConan(ConanFile):
-    build_requires = "gtest/1.13.0"
     name = "portable_concurrency"
     version = "0.11.0"
     license = "CC0"
     url = "https://github.com/VestniK/portable_concurrency"
     description = "Portable future/promise implemenattion close to ConcurrencyTS specification"
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False],
-               "no_deprecated": [True, False]}
-    default_options = {"shared": False,
-                       "no_deprecated": False}
+    options = {
+        "fPIC": [True, False],
+        "shared": [True, False],
+        "no_deprecated": [True, False],
+    }
+    default_options = {
+        "fPIC": True,
+        "shared": False,
+        "no_deprecated": False,
+    }
     exports_sources = "*", "!build/*"
 
     def layout(self):
         cmake_layout(self)
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            self.options.rm_safe("fPIC")
+
+    def requirements(self):
+        self.requires("gtest/1.13.0", private=True)
+        self.requires("asio/1.28.0", private=True)
 
     def generate(self):
         tc = CMakeToolchain(self)
@@ -30,14 +43,14 @@ class PortableconCurrencyConan(ConanFile):
             cmake.definitions['PC_DEV_BUILD'] = True
         if self.options.no_deprecated:
             cmake.definitions['PC_NO_DEPRECATED'] = True
-        cmake.configure(source_folder="./")
+        cmake.configure()
         cmake.build()
-        if not cross_building(self.settings):
+        if not cross_building(self):
             cmake.test()
-        cmake.install()
 
     def package(self):
-        pass
+        cmake = CMake(self)
+        cmake.install()
 
     def package_info(self):
         self.cpp_info.libs = ["portable_concurrency"]
